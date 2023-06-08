108e71a21ffa6589c05e2c88d7dc0ba30e9bfe1e
Jian-Wuyou <dramante@up.edu.ph>
2023-06-09 00:44:51 +0800

    feat: everything

---
+++ app/app.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+
+from app.prediction_model import Model, feature_cols, response_col
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config.from_prefixed_env()
+
+with open("../Sleep_Efficiency.csv") as f:
+    model = Model(f.read())
+
+
+@app.post("/predict")
+def predict():
+    sleep_eff = model.predict(request.json)
+
+    print(f"{sleep_eff = }")
+    return {"sleep_efficiency": sleep_eff}, 200
+
+
+@app.get("/")
+def index():
+    """Displays a form where user can input their model"""
+    form_entries = "".join(
+        f'<p><label for="{f}">{f}</label>'
+        f'<input type="{f}" name="{f}" id="{f}"/></p>'
+        for f in feature_cols
+    )
+    return render_template(
+        "index.html", form_entries=form_entries, response_col=response_col
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
